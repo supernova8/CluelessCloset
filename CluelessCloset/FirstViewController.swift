bf7a58461e129d8acbb1ec8d3e19b4775620f129
Sonova Middleton <puddin88@gmail.com>
2015-08-04 16:44:53 -0400

    Initial Commit

---
+++ CluelessCloset/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CluelessCloset
+//
+//  Created by Sonova Middleton on 8/4/15.
+//  Copyright (c) 2015 supernova8productions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CluelessCloset/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CluelessCloset
+//
+//  Created by Sonova Middleton on 8/4/15.
+//  Copyright (c) 2015 supernova8productions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
